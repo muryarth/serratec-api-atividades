11f5e30c78d93bf636edf011d9b4b445eb1f14c9
muryarth <arthur.mury@outlook.com>
2024-05-16 17:12:14 -0300

    feat: subindo ajustes no exercício demo

--- demo/src/main/java/org/serratec/backend/controller/AlunoController.java
+++ demo/src/main/java/org/serratec/backend/controller/AlunoController.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.serratec.backend.domain.Aluno;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -42,4 +46,27 @@ public class AlunoController {
 		lista.add(aluno);
 		return aluno;
 	}
+	
+	@DeleteMapping("/{matricula}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long matricula) {
+        for (int i=0; i<lista.size(); i++) {
+            if (lista.get(i).getMatricula().equals(matricula)) {
+                lista.remove(i);
+                break;
+            }
+        }
+    }
+
+    @PutMapping("/{matricula}")
+    public Aluno atualizar(@RequestBody Aluno aluno, @PathVariable Long matricula) {
+        for (int i=0; i<lista.size(); i++) {
+            if (lista.get(i).getMatricula().equals(matricula)) {
+                Aluno a = new Aluno(matricula, aluno.getNome(), aluno.getTelefone());
+                lista.set(i, a);
+                return a;
+            }
+        }
+        return null;
+    }
 }
